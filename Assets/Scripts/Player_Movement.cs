ba46b1ad13f6e975228898a72fd629d7fea5f474
micassta <0233767@up.edu.mx>
2025-05-19 16:38:11 -0600

    YA SE PUDO AGACHAR Y SALTAR
    Ahora, el player puede agacharse y no se puede parar si hay algo arriba de el, y en vez de una linea de raycast, es una caja que abarca todo el jugador. Esto hace mas realista las fisicas de Spelunky...digo CasteMikey

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -16,9 +16,11 @@ public class Player_Movement : MonoBehaviour
     public Vector3 playerScale;
 
     public Animator animator;
-    public float raycastDistance = 0.5f;
+    public float raycastDistance_hit_floor = 0.5f;
+    public float raycastDistance_hit_roof = 0.38f;
     public LayerMask floorLayer;
     private bool onFloor;
+    private bool noRoof;
 
     ///////////
     public Agarrar_Objeto agarrarObjeto;
@@ -26,7 +28,8 @@ public class Player_Movement : MonoBehaviour
     ///////////
 
     ///
-    public Vector2 sizeBoxOnFloor = new Vector2 (1f, 1f);
+    public Vector2 sizeBoxOnFloor = new Vector2 (0.76f, 0.6f);
+    public Vector2 sizeBoxToRoof = new Vector2 (0.78f, 0.76f);
     ///
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -56,9 +59,15 @@ public class Player_Movement : MonoBehaviour
 
         //RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, floorLayer);
         //onFloor = hit.collider == true;
-        Debug.Log(transform.position);
-        RaycastHit2D hit_floor = Physics2D.BoxCast(transform.position, sizeBoxOnFloor, 0f, Vector2.down, raycastDistance, floorLayer);
+        //Debug.Log(transform.position);
+
+        RaycastHit2D hit_floor = Physics2D.BoxCast(transform.position, sizeBoxOnFloor, 0f, Vector2.down, raycastDistance_hit_floor, floorLayer);
         onFloor = hit_floor.collider != null;
+
+        RaycastHit2D hit_roof = Physics2D.BoxCast(transform.position, sizeBoxToRoof, 0f, Vector2.up, raycastDistance_hit_roof, floorLayer);
+        noRoof = hit_roof.collider == false;
+
+
         //animator.SetBool("onFloor", onFloor);
 
         //animator.SetFloat("movement", movement.x);
@@ -78,7 +87,7 @@ public class Player_Movement : MonoBehaviour
         ///////////
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            //Debug.Log("TOY AGACHADO");
+            Debug.Log("TOY AGACHADO");
             agarrarObjeto.AgarrarSoltar();  //Funcion del Script Agarrar_Objeto
             bc.offset = new Vector2(0f, -0.25f);
             bc.size = new Vector2(1f, 0.5f);
@@ -88,11 +97,14 @@ public class Player_Movement : MonoBehaviour
 
         else
         {
-            //Debug.Log("TOY DE PIE");
-            bc.offset = new Vector2(0f, 0f);
-            bc.size = new Vector2(1f, 1f);
-            velocity = 5f;
-            jumpForce = 5f;
+            if (noRoof)
+            {
+                Debug.Log("TOY DE PIE");
+                bc.offset = new Vector2(0f, 0f);
+                bc.size = new Vector2(1f, 1f);
+                velocity = 5f;
+                jumpForce = 5f;
+            }
         }
         ///////////
 
@@ -111,15 +123,20 @@ public class Player_Movement : MonoBehaviour
         Gizmos.color = UnityEngine.Color.blue;
 
         Vector2 origin = transform.position;
-        Vector2 direction = Vector2.down;
+        Vector2 direction_hit_floor = Vector2.down;
 
-        // Dibuja el box inicial (desde donde se lanza)
+        //BoxOriginal
         Gizmos.DrawWireCube(origin, sizeBoxOnFloor);
 
-        // Dibuja el box final (posición a la que llegará)
-        Vector2 endPos = origin + direction * raycastDistance;
+        //Box del Onfloor
+        Vector2 endPos_hit_floor = origin + direction_hit_floor * raycastDistance_hit_floor;
         Gizmos.color = UnityEngine.Color.red;
-        Gizmos.DrawWireCube(endPos, sizeBoxOnFloor);
+        Gizmos.DrawWireCube(endPos_hit_floor, sizeBoxOnFloor);
+
+        Vector2 direction_hit_roof = Vector2.up;
+        Vector2 endPos_hit_roof = origin + direction_hit_roof * raycastDistance_hit_roof;
+        Gizmos.color = UnityEngine.Color.green;
+        Gizmos.DrawWireCube(endPos_hit_roof, sizeBoxToRoof);
     }
 
 }
